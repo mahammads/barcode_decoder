211cfffa7ac088e11cce5f875d0eefa11de69331
mahammads <106377568+mahammads@users.noreply.github.com>
2023-09-10 21:33:11 +0530

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,3 @@
+dynamo_key = "t0075oQAAAEGRpoU+HCIjIB/A3iIMBheBwi7oV9v14lMQUoEWgRcBYjeQ0rHGvIpt2y6C3XZC6ql6ZV8refCPtne+unyPSepa2Av4viLS"
+tesseract = r"C:\Users\sarwa\AppData\Local\Tesseract-OCR\tesseract.exe"
+poppler_path = r"C:\Users\sarwa\Documents\poppler-22.04.0\Library\bin"
